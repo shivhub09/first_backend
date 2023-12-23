0ee09983c07e6001ee11028e327055bc52475f30
shivhub09 <shivnagori2020@gmail.com>
2023-12-24 00:13:34 +0530

    changes

--- main.py
+++ main.py
@@ -4,19 +4,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Load your TensorFlow Lite model
 interpreter = tf.lite.Interpreter(model_path='your_model.tflite')
 interpreter.allocate_tensors()
 
-# Get input and output details of the model
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 def load_and_preprocess_image(image_data, target_size=(150, 150)):
     img = tf.image.decode_jpeg(image_data, channels=3)
     img = tf.image.resize(img, target_size)
-    img = tf.cast(img, tf.float32) / 255.0  # Normalize pixel values
-    img = tf.expand_dims(img, axis=0)  # Add batch dimension
+    img = tf.cast(img, tf.float32) / 255.0 
+    img = tf.expand_dims(img, axis=0) 
     return img
 
 @app.route('/predict', methods=['POST'])
@@ -28,10 +26,8 @@ def predict():
         base64_image = request.json['image']
         image_data = base64.b64decode(base64_image)
 
-        # Preprocess the image using the provided function
         input_img = load_and_preprocess_image(image_data)
 
-        # Set the image data as input to the model
         interpreter.set_tensor(input_details[0]['index'], input_img)
         interpreter.invoke()
 
