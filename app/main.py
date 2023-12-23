416df7dae71a6db168b74569d7295c3b92cc6838
shivhub09 <shivnagori2020@gmail.com>
2023-12-24 01:21:42 +0530

    changes 33

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Load the TFLite model
 interpreter = tf.lite.Interpreter(model_path='your_model.tflite')
 interpreter.allocate_tensors()
 
@@ -11,16 +12,19 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 def load_and_preprocess_image(image_data, target_size=(150, 150)):
-    img = tf.image.decode_jpeg(image_data, channels=3)
-    img = tf.image.resize(img, target_size)
-    img = tf.cast(img, tf.float32) / 255.0 
-    img = tf.expand_dims(img, axis=0) 
-    return img
+    try:
+        img = tf.image.decode_jpeg(image_data, channels=3)
+        img = tf.image.resize(img, target_size)
+        img = tf.cast(img, tf.float32) / 255.0 
+        img = tf.expand_dims(img, axis=0) 
+        return img
+    except Exception as e:
+        raise ValueError("Error processing image: {}".format(str(e)))
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        if 'image' not in request.json:
+        if not request.json or 'image' not in request.json:
             return jsonify({'error': 'No image data found'})
 
         base64_image = request.json['image']
@@ -36,6 +40,10 @@ def predict():
 
         return jsonify({'prediction': output.tolist()})
     
+    except ValueError as ve:
+        return jsonify({'error': str(ve)})
     except Exception as e:
-        return jsonify({'error': str(e)})
+        # Log the error
+        app.logger.error("Prediction error: %s", str(e))
+        return jsonify({'error': 'An error occurred during prediction'})
 
